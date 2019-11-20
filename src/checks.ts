75e0af29e202fcb01c7530ba0b28a36afb48f1fe
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-11-20 19:17:36 +0900

    実装フェイズで対応すべきTODOをコメントした。

--- src/checks.ts
+++ src/checks.ts
@@ -7,6 +7,7 @@ export const Checks = {
   /**
    * 値が数値か否かを返す
    * @param value
+   * @todo `value` を　`unknown` 型とする
    * @todo `typeof` を使わない実装にする
    */
   isNumber(value: any): value is number {
