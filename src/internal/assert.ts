470338fe4ee6fc97a7a3c3fbb09c4db1b7694723
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-11-21 18:24:11 +0900

    `assert()` を実装した。

--- src/internal/assert.ts
+++ src/internal/assert.ts
@@ -2,8 +2,13 @@
  * @hidden 指定の条件でアサートする
  * @param condition
  * @param errorMessage
+ * @throw `condition` が Falthy ならば `errorMessage` を内容とする例外を投げる
  */
 export function assert(
   condition: unknown,
   errorMessage?: string
-): asserts condition {}
+): asserts condition {
+  if (!condition) {
+    throw new Error(errorMessage)
+  }
+}
