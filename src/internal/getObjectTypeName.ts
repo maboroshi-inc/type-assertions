e3460a877ee2b42eda50fee11be84ff1e35cd4ba
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-11-21 12:24:29 +0900

    `getObjectTypeName()` を実装した。

--- src/internal/getObjectTypeName.ts
+++ src/internal/getObjectTypeName.ts
@@ -1,5 +1,8 @@
 /**
  * @hidden オブジェクトの型名を文字列で返す
+ * @see https://qiita.com/amamamaou/items/ef0b797156b324bb4ef3#objectprototypetostringcall-%E3%82%92%E4%BD%BF%E3%81%86
  * @param value 型名を得たいオブジェクト
  */
-export function getObjectTypeName(value: unknown) {}
+export function getObjectTypeName(value: unknown): string {
+  return Object.prototype.toString.call(value)
+}
