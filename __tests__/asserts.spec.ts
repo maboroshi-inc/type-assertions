6f4f0b88c42f494784b7e8fb6f3a939959d371d7
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-11-21 19:47:07 +0900

    `Assert.isNumber()` のテストコードを定義した。

--- __tests__/asserts.spec.ts
+++ __tests__/asserts.spec.ts
@@ -1,9 +1,20 @@
-describe('サンプルテストコード', () => {
-  it('通るテスト', () => {
-    expect(true).toBeTruthy()
-  })
+import Asserts from '../src/asserts'
+import Checks from '../src/checks'
 
-  it.skip('落ちるテスト', () => {
-    expect(true).toBeFalsy()
+describe('Asserts API', () => {
+  describe('isNumber()', () => {
+    it('`Checks.isNumber()` を呼び出す', () => {
+      const spy = jest.spyOn(Checks, 'isNumber')
+      const main = () => Asserts.isNumber(123)
+
+      main()
+      expect(spy).toHaveBeenCalledWith(123)
+
+      spy.mockRestore()
+    })
+
+    it('例外を投げる', () => {
+      expect(() => Asserts.isNumber(null)).toThrowError('value is not a number')
+    })
   })
 })
