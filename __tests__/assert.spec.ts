fe3bd8502642bdb7e9ae18e5444b290684ccd62a
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-11-21 18:21:54 +0900

    `assert()` のテストケースを定義した。

--- __tests__/assert.spec.ts
+++ __tests__/assert.spec.ts
@@ -4,4 +4,10 @@ describe('assert()', () => {
   it('`void` を返す', () => {
     expect(assert(true)).toBeUndefined()
   })
+
+  it('例外を投げる', () => {
+    expect(() => assert(false, 'A DEFINED ERROR MESSAGE')).toThrowError(
+      'A DEFINED ERROR MESSAGE'
+    )
+  })
 })
