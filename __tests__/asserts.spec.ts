6f5f18760a24fdbb3a7181636fc2c6de1bad11e0
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-11-22 13:10:09 +0900

    Asserts API のテストコードを整理した。

--- __tests__/asserts.spec.ts
+++ __tests__/asserts.spec.ts
@@ -2,19 +2,35 @@ import Asserts from '../src/asserts'
 import Checks from '../src/checks'
 
 describe('Asserts API', () => {
+  const createAssertion = (assertion: (value: unknown) => void) => (
+    value: unknown
+  ) => assertion(value)
+
   describe('isNumber()', () => {
-    it('`Checks.isNumber()` を呼び出す', () => {
-      const spy = jest.spyOn(Checks, 'isNumber')
-      const main = () => Asserts.isNumber(123)
+    const assertion = createAssertion(Asserts.isNumber)
+    let spy: jest.SpyInstance
 
-      main()
-      expect(spy).toHaveBeenCalledWith(123)
+    beforeEach(() => {
+      spy = jest.spyOn(Checks, 'isNumber')
+    })
 
+    afterEach(() => {
       spy.mockRestore()
     })
 
+    it('`Checks.isNumber()` を呼び出す', () => {
+      assertion(123)
+      expect(spy).toHaveBeenCalledWith(123)
+    })
+
+    it('`void` を返す', () => {
+      expect(assertion(123)).toBeUndefined()
+      expect(spy).toHaveReturnedWith(true)
+    })
+
     it('例外を投げる', () => {
-      expect(() => Asserts.isNumber(null)).toThrowError('value is not a number')
+      expect(() => assertion(null)).toThrowError('value is not a number')
+      expect(spy).toHaveReturnedWith(false)
     })
   })
 })
