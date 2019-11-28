c629ab5adcbb0fb24f20ff2111f683be9ad097d1
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-11-28 13:33:23 +0900

    Checks API のテストケースに `getObjectTypeName()` を呼び出すことを追加した。

--- __tests__/checks.spec.ts
+++ __tests__/checks.spec.ts
@@ -1,7 +1,25 @@
 import Checks from '../src/checks'
 
 describe('Checks API', () => {
+  let getObjectTypeNameSpy = jest.spyOn(
+    require('../src/internal/getObjectTypeName'),
+    'getObjectTypeName'
+  )
+
+  beforeEach(() => {
+    getObjectTypeNameSpy.mockClear()
+  })
+
+  afterAll(() => {
+    getObjectTypeNameSpy.mockRestore()
+  })
+
   describe('isNumber()', () => {
+    it('`getObjectTypeName()` を呼び出す', () => {
+      Checks.isNumber(123)
+      expect(getObjectTypeNameSpy).toBeCalledWith(123)
+    })
+
     it('`true` を返す', () => {
       expect(Checks.isNumber(NaN)).toBe(true)
       expect(Checks.isNumber(123)).toBe(true)
