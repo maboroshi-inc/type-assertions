df231a8ce127e62ec117d3bd7343fabeef80ac6a
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-11-28 13:39:53 +0900

    Asserts API のテストコードを整理した。

--- __tests__/asserts.spec.ts
+++ __tests__/asserts.spec.ts
@@ -8,29 +8,29 @@ describe('Asserts API', () => {
 
   describe('isNumber()', () => {
     const assertion = createAssertion(Asserts.isNumber)
-    let spy: jest.SpyInstance
+    let checksAPISpy = jest.spyOn(Checks, 'isNumber')
 
     beforeEach(() => {
-      spy = jest.spyOn(Checks, 'isNumber')
+      checksAPISpy.mockClear()
     })
 
-    afterEach(() => {
-      spy.mockRestore()
+    afterAll(() => {
+      checksAPISpy.mockRestore()
     })
 
     it('`Checks.isNumber()` を呼び出す', () => {
       assertion(123)
-      expect(spy).toHaveBeenCalledWith(123)
+      expect(checksAPISpy).toHaveBeenCalledWith(123)
     })
 
     it('`void` を返す', () => {
       expect(assertion(123)).toBeUndefined()
-      expect(spy).toHaveReturnedWith(true)
+      expect(checksAPISpy).toHaveReturnedWith(true)
     })
 
     it('例外を投げる', () => {
       expect(() => assertion(null)).toThrowError('value is not a number')
-      expect(spy).toHaveReturnedWith(false)
+      expect(checksAPISpy).toHaveReturnedWith(false)
     })
   })
 })
